b8b903f2d485e91f19f0bfb223e50d4df0ae7f84
Devina Grace Felicia <98725230+deviinagrace@users.noreply.github.com>
2023-10-26 21:18:20 +0700

    Update cart2.php

--- cart2.php
+++ cart2.php
@@ -11,12 +11,12 @@
         <div class="cart">
             <div class="products">
                 <div class="product">
-                    <img src="shoes.jpg">
+                    <img src="image/Coklat.jpg">
                     <div class="product-info">
-                        <h3 class="product-name">New Shoes</h3>
-                        <h4 class="product-price">₹ 1,000</h4>
-                        <h4 class="product-offer">50%</h4>
-                        <p class="product-quantity">Qnt: <input value="1" name="">
+                        <h3 class="product-name">Coklat</h3>
+                        <h4 class="product-price">Rp 3.000</h4>
+                        <h4 class="product-offer">Satuan</h4>
+                        <p class="product-quantity">Qty: <input value="1" name="">
                         <p class="product-remove">
                             <i class="fa fa-trash" aria-hidden="true"></i>
                             <span class="remove">Remove</span>
@@ -24,12 +24,38 @@
                     </div>
                 </div>
                 <div class="product">
-                    <img src="bag1.jpg">
+                    <img src="image/Nanas.jpg">
                     <div class="product-info">
-                        <h3 class="product-name">New Bag</h3>
-                        <h4 class="product-price">₹ 2,000</h4>
-                        <h4 class="product-offer">40%</h4>
-                        <p class="product-quantity">Qnt: <input value="1" name="">
+                        <h3 class="product-name">Besar Nanas</h3>
+                        <h4 class="product-price">Rp 18.000</h4>
+                        <h4 class="product-offer">-Isi 30pcs-</h4>
+                        <p class="product-quantity">Qty: <input value="1" name="">
+                        <p class="product-remove">
+                            <i class="fa fa-trash" aria-hidden="true"></i>
+                            <span class="remove">Remove</span>
+                        </p>
+                    </div>
+                </div>
+                <div class="product">
+                    <img src="image/Keju.jpg">
+                    <div class="product-info">
+                        <h3 class="product-name">Besar Keju</h3>
+                        <h4 class="product-price">Rp 18.000</h4>
+                        <h4 class="product-offer">-Isi 30pcs-</h4>
+                        <p class="product-quantity">Qty: <input value="1" name="">
+                        <p class="product-remove">
+                            <i class="fa fa-trash" aria-hidden="true"></i>
+                            <span class="remove">Remove</span>
+                        </p>
+                    </div>
+                </div>
+                <div class="product">
+                    <img src="image/Kacang.jpg">
+                    <div class="product-info">
+                        <h3 class="product-name">Kecil Kacang</h3>
+                        <h4 class="product-price">Rp 12.000</h4>
+                        <h4 class="product-offer">-Isi 20pcs-</h4>
+                        <p class="product-quantity">Qty: <input value="1" name="">
                         <p class="product-remove">
                             <i class="fa fa-trash" aria-hidden="true"></i>
                             <span class="remove">Remove</span>
@@ -40,15 +66,11 @@
             <div class="cart-total">
                 <p>
                     <span>Total Price</span>
-                    <span>₹ 3,000</span>
+                    <span>Rp 51.000</span>
                 </p>
                 <p>
                     <span>Number of Items</span>
-                    <span>2</span>
-                </p>
-                <p>
-                    <span>You Save</span>
-                    <span>₹ 1,000</span>
+                    <span>4</span>
                 </p>
                 <a href="#">Proceed to Checkout</a>
             </div>
